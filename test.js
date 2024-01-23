75538146c70dfd7ff388a5530c876299037f0986
Jenna Qian <jiayin1996@gmail.com>
2024-01-22 22:34:11 -0500

    remove pseudocode

--- test.js
+++ test.js
@@ -9,23 +9,17 @@ const clickBtnSelector = 'button[class="component-response-multiple-choice__opti
 const unclickBtnSelector = 'button[class="component-response-multiple-choice__option__undo"]'
 
 const main = async () => {
-    // Launch browser with UI
     const browser = await puppeteer.launch({headless:false});
 
-    // Opens a new page 
     const page = await browser.newPage();
 
     try {
-            // Navigate to poll page 
             await page.goto(poll, {waitUntil: "networkidle0"});
         
-            // Enter name and wait 2 seconds 
             await page.type(nameSelector, nameText)
         
-            // Click continue to bypass modal 
             await page.click(continueSelector)
         
-            // Verified buttons are clickable and correlate with the response page
             await page.evaluate(async (clickSelector, unclickSelector) => {
                 const clickButtons = document.querySelectorAll(clickSelector);
                 const unclickButtons = document.querySelectorAll(unclickSelector);
@@ -35,29 +29,29 @@ const main = async () => {
                     const unclickButton = unclickButtons[i];
 
                     clickButton.click();
+                    await new Promise(resolve => setTimeout(resolve, 1000));
 
                     unclickButton.click();
+                    await new Promise(resolve => setTimeout(resolve, 1000));
                 }
-
                 console.log('All buttons have been clicked')
             }, clickBtnSelector, unclickBtnSelector)
 
 
-            // Click on the button "Malevolent" for word of the day
-            // await page.evaluate((selector)=> {
-            //     const buttons = document.querySelectorAll(selector)
+            await page.evaluate((selector)=> {
+                const buttons = document.querySelectorAll(selector)
         
-            //     if(buttons.length >= 3){
-            //         buttons[2].click();
-            //     } else {
-            //         console.log('There are less than three buttons matching the selector.')
-            //     }
-            // }, clickBtnSelector)
+                if(buttons.length >= 3){
+                    buttons[2].click();
+                } else {
+                    console.log('There are less than three buttons matching the selector.')
+                }
+            }, clickBtnSelector)
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
         
-            // Navigate to response page for observation 
             await page.goto(resultsPage)
         
-            // Array of div selectors 
             const divSelectors = [
                 ['span[id="keyword_poll_option_51559428"]>div[class="font-semibold"]','div#count_label_poll_option_51559428'],
                 ['span[id="keyword_poll_option_51559429"]>div[class="font-semibold"]','div#count_label_poll_option_51559429'],
@@ -68,7 +62,6 @@ const main = async () => {
             let totalValue = 0;
             let percentages = [];
         
-            // Iterate over each button 
             for (const [choiceSelector, divSelector] of divSelectors){
                 await page.waitForSelector(divSelector);
         
@@ -79,12 +72,13 @@ const main = async () => {
                 percentages.push({spanValue, divValue})
             }
         
-            // Calculate percentages log the results 
             for(const {spanValue, divValue} of percentages){
                 const decimalNumber = divValue / totalValue;
                 const percentageValue = (decimalNumber * 100).toFixed(0);
                 console.log(`${spanValue} : ${percentageValue}%, ${divValue} votes out of ${totalValue}`)
             }
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
 
     } catch (error) {
         console.error('Error, unable to reach URL')
