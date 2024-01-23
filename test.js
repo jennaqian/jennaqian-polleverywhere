1524e7c3961c604d0a3277ae2be62aaebfa9c4e8
Jenna Qian <jiayin1996@gmail.com>
2024-01-22 21:23:16 -0500

    Automated the selection of the Malevolent button for the word of the day

--- test.js
+++ test.js
@@ -17,6 +17,18 @@ const main = async () => {
 
     // Click continue to bypass modal 
     await page.click('button[data-controller="material--mdc-ripple"]')
+
+    // Click on the button "Malevolent" for word of the day
+    await page.evaluate((selector)=> {
+        const buttons = document.querySelectorAll(selector)
+
+        if(buttons.length >= 3){
+            buttons[2].click();
+        } else {
+            console.log('There are less than three buttons matching the selector.')
+        }
+    }, 'button[class="component-response-multiple-choice__option__vote"]')
+
 }
 
 main();
